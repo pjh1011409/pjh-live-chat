33e3090d1fa7a35cbc87151d4f7a9acb53fc0450
JeongHo Park <81337674+pjh1011409@users.noreply.github.com>
2023-02-06 18:31:29 +0900

    Update useSocket.ts

--- client/hooks/useSocket.ts
+++ client/hooks/useSocket.ts
@@ -3,7 +3,7 @@ import { io, Socket } from 'socket.io-client';
 
 const backUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://sleact.nodebird.com'
+    ? ''
     : 'http://localhost:3095';
 
 const sockets: { [key: string]: Socket } = {};
